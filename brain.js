70169a9a182bf23aed1046aa660778d2ae1c62f0
Your GitHub Username <Your GitHub Email>
2024-05-05 19:47:35 -0700

    BRAIN BITCH HELL YEAAAAAAAAAAAAAAAAAAAAAAA

--- brain.js
+++ brain.js
@@ -1,9 +1,10 @@
 class RedNode{
     //compare symbol >, <, =
-    constructor(weight, compareNodes, compareSymbol){
+    constructor(weight, compareNodes, compareSymbol, output){
         this.weight = weight
         this.compareNodes = compareNodes
         this.compareSymbol = compareSymbol
+        this.output = output
     }
 
     Node(v1, v2){
@@ -38,23 +39,53 @@ class Brain{
     }
 }
 
-let brain = new Brain([5,2,4,2])
+let brain = new Brain([90,500,20,2000])
 brain.networkSetup()
 console.log(brain.network)
 
-let red1 = new RedNode(Math.random(), [[0,Math.floor(Math.random() * 4)],[0,Math.floor(Math.random() * 4)]],Math.floor(Math.random() * 3))
-let red2 = new RedNode(Math.random(), [[0,Math.floor(Math.random() * 4)],[0,Math.floor(Math.random() * 4)]],Math.floor(Math.random() * 3))
-let red3 = new RedNode(Math.random(), [[0,Math.floor(Math.random() * 4)],[0,Math.floor(Math.random() * 4)]],Math.floor(Math.random() * 3))
-let red4 = new RedNode(Math.random(), [[0,Math.floor(Math.random() * 4)],[0,Math.floor(Math.random() * 4)]],Math.floor(Math.random() * 3))
-brain.network.push([red1, red2, red3, red4])
+let nextSet = []
 
-console.log(red1)
-console.log(red2)
-console.log(red3)
-console.log(red4)
+let outputs = ["stopLeft", "Left", "stopRight", "Right", "Jump", "stopJump"]
+
+for(let i = 0; i < 6; i++){
+    let red = new RedNode(Math.random(), [[0,Math.floor(Math.random() * 4)],[0,Math.floor(Math.random() * 4)]], Math.floor(Math.random() * 3), outputs[Math.floor(Math.random() * 6)])
+    nextSet.push(red)
+}
+
+brain.network.push(nextSet)
+
+for(let i = 0; i < nextSet.length; i++){
+    console.log(nextSet[i])
+}
+
+let outputPercent = []
 
 for(let i = 0; i < brain.network[1].length; i++){
     let compare1 = brain.network[brain.network[1][i].compareNodes[0][0]][brain.network[1][i].compareNodes[0][1]]
     let compare2 = brain.network[brain.network[1][i].compareNodes[1][0]][brain.network[1][i].compareNodes[1][1]]
     console.log(brain.network[1][i].Node(compare1, compare2))
+    if(brain.network[1][i].Node(compare1, compare2)){
+        outputPercent.push({output : brain.network[1][i].output, weight: brain.network[1][i].weight})
+    }
+}
+console.log(outputPercent)
+let totalWeight = 0
+for(let i in outputPercent){
+    totalWeight += outputPercent[i].weight
+}
+
+let chosenWeight = Math.random() * totalWeight
+console.log(chosenWeight)
+
+for(let i = 0; i < outputPercent.length; i++){
+    if(outputPercent[i].weight > chosenWeight){
+        console.log("in")
+        console.log(outputPercent[i].output)
+        break
+    }
+    else{
+        chosenWeight -= outputPercent[i].weight
+        console.log("LOWERED TOTAL WEIGH")
+        console.log(chosenWeight)
+    }
 }
